5d5782946d47c3e84ac1e7f62cbbdd409a85a232
cklewar <cklewar@t-online.de>
2023-12-06 15:23:20 +0100

    add gitlab update

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ import boto3
 import botocore
 import botocore.config
 import botocore.errorfactory
+import gitlab
 
 START_LEAF = "{urn:oasis:names:tc:SAML:2.0:assertion}Attribute"
 SECOND_LEAF = "{urn:oasis:names:tc:SAML:2.0:assertion}AttributeValue"
@@ -17,10 +18,12 @@ SESSION_ROLE = "Session Role"
 AWS_DEFAULT_CONFIG_PATH = "~/.aws/config"
 AWS_DEFAULT_CREDENTIALS_PATH = "~/.aws/credentials"
 SAML_DEFAULT_SESSION_DURATION = 43200
+AWS_CREDENTIAL_VARS = ["aws_access_key_id", "aws_session_token", "aws_secret_access_key"]
 Data = namedtuple("Data", "role_arn idp_arn")
+Saml = namedtuple("Saml", "aws_access_key_id aws_session_token aws_secret_access_key")
 
 
-def run(profile=None, region: str = None, idp_arn: str = None, role_arn: str = None, saml: str = None, write_to_file: bool = False, export_to_env: bool = False, print_to_stdout: bool = False):
+def run(profile=None, region: str = None, idp_arn: str = None, role_arn: str = None, saml: str = None, write_to_file: bool = False, export_to_env: bool = False, print_to_stdout: bool = False) -> Saml:
     profile_name = profile or os.environ.get("AWS_PROFILE", "default")
     region_name = region or os.environ.get("AWS_DEFAULT_REGION", None)
     section_name = (profile_name if profile_name == "default" else f"profile {profile_name}")
@@ -59,18 +62,21 @@ def run(profile=None, region: str = None, idp_arn: str = None, role_arn: str = N
             if write_to_file:
                 with open(cred_path, "w+") as f:
                     cred.write(f)
-                    print("Credentials saved for {}. Expire {}.".format(profile_name, response["Credentials"]["Expiration"]))
+                    print("Credentials saved to file for {}. Expire {}.".format(profile_name, response["Credentials"]["Expiration"]))
             elif export_to_env:
                 os.environ['AWS_ACCESS_KEY_ID'] = cred.get(profile_name, "aws_access_key_id")
                 os.environ['AWS_SESSION_TOKEN'] = cred.get(profile_name, "aws_session_token")
                 os.environ['AWS_SECRET_ACCESS_KEY'] = cred.get(profile_name, "aws_session_token")
+                print("Credentials exported to env variables for {}. Expire {}.".format(profile_name, response["Credentials"]["Expiration"]))
+            elif print_to_stdout:
+                print("AWS_ACCESS_KEY_ID:", cred.get(profile_name, "aws_access_key_id"))
+                print("AWS_SESSION_TOKEN:", cred.get(profile_name, "aws_session_token"))
+                print("AWS_SECRET_ACCESS_KEY:", cred.get(profile_name, "aws_secret_access_key"))
+                print("Credentials for {}. Expire {}.".format(profile_name, response["Credentials"]["Expiration"]))
 
-                if print_to_stdout:
-                    print("AWS_ACCESS_KEY_ID:", os.getenv("AWS_ACCESS_KEY_ID"))
-                    print("AWS_SESSION_TOKEN:", os.getenv("AWS_SESSION_TOKEN"))
-                    print("AWS_SECRET_ACCESS_KEY:", os.getenv("AWS_SECRET_ACCESS_KEY"))
-
-                print("Credentials exported for {}. Expire {}.".format(profile_name, response["Credentials"]["Expiration"]))
+            return Saml(aws_access_key_id=cred.get(profile_name, "aws_access_key_id"),
+                        aws_session_token=cred.get(profile_name, "aws_session_token"),
+                        aws_secret_access_key=cred.get(profile_name, "aws_secret_access_key"))
 
     except botocore.errorfactory.ClientError as ete:
         print(ete)
@@ -90,6 +96,29 @@ def get(saml: str) -> Data:
     return _data
 
 
+def update(pat: str = None, group: str = None, saml: Saml = None):
+    gl = gitlab.Gitlab(private_token=pat)
+    g = gl.groups.get(gl.groups.list(search=group, get_all=True)[0].id)
+
+    for _p in g.projects.list(get_all=True):
+        if _p.name == "sense8":
+            print(f"Get group <{group}> project <{_p.name}> object...")
+            p = gl.projects.get(_p.id)
+            print(f"Get group <{group}> project <{p.name}> object -> Done")
+
+            for variable in AWS_CREDENTIAL_VARS:
+                try:
+                    print(f"Update variable <{variable}>...")
+                    _variable = p.variables.get(variable.upper())
+                    _variable.value = getattr(saml, variable)
+                    _variable.save()
+                    print(f"Update variable <{variable}> -> DONE")
+                except (gitlab.exceptions.GitlabHttpError, gitlab.exceptions.GitlabGetError) as err:
+                    print(f"Update project <{p.name}> level variable <{variable}> failed with error: {err.error_message}")
+                except AttributeError as ae:
+                    print(ae)
+
+
 if __name__ == '__main__':
     print("SAML ROLE_ARN and IDP_ARN extractor")
     parser = argparse.ArgumentParser()
@@ -98,18 +127,21 @@ if __name__ == '__main__':
     parser.add_argument("-e", "--environment", help="export environment vars", type=bool)
     parser.add_argument("-w", "--write", help="write credentials file", type=bool)
     parser.add_argument("-p", "--print", help="print credentials to stdout", type=bool)
+    parser.add_argument("-u", "--update", help="update credentials in gitlab", type=bool)
+    parser.add_argument("-a", "--pat", help="Gitlab PAT", type=str)
     args = parser.parse_args()
+    data = None
 
     if args.file:
         with open(args.file, "r") as fp:
             raw = fp.read()
         data = get(base64.b64decode(raw).decode("utf-8"))
-        run(idp_arn=data.idp_arn, role_arn=data.role_arn, saml=raw, write_to_file=args.write)
 
     if args.string:
         data = get(base64.b64decode(args.string).decode("utf-8"))
-        run(idp_arn=data.idp_arn, role_arn=data.role_arn, saml=args.string, export_to_env=args.environment)
 
-    if args.print:
-        data = get(base64.b64decode(args.string).decode("utf-8"))
-        run(idp_arn=data.idp_arn, role_arn=data.role_arn, saml=args.string, print_to_stdout=args.print)
+    if data:
+        saml = run(idp_arn=data.idp_arn, role_arn=data.role_arn, saml=args.string, write_to_file=args.write, export_to_env=args.environment, print_to_stdout=args.print)
+
+        if args.update:
+            update(pat=args.pat, group="regression", saml=saml)
